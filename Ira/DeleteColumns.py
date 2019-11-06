745322255825c9ef3b42cb576711efc2f913209e
TommyNiv <tnivon@gmail.com>
2019-11-06 15:53:41 -0500

    Add Tomas File

--- Ira/DeleteColumns.py
+++
@@ -1,5 +0,0 @@
-#import data set
-iowa_train = pd.read_csv('train.csv')
-
-#remove some columns
-iowa_train_filtered = iowa_train.drop(['Alley', 'PoolQC', 'Fence', 'PoolArea', 'MiscFeature'], axis = 1)
--- combine.py
+++
@@ -1,3 +0,0 @@
-#This py file is for putting everyone's work together.
-#Please update on your own py file under your folder first and try it out 
-#before putting it up here. 
